2cce47a3f418f6ebd68665a9c2d8335dc53fa27d
RichTillis <rich.tillis@gmail.com>
2015-12-04 12:35:03 -0700

    testing commit

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	return "Hello Beautiful World!"
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
