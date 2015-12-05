66d44f8c79d249f7390716f844c1a2d1dd257c45
RichTillis <rich.tillis@gmail.com>
2015-12-05 07:42:17 -0700

    db definitation update

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ class GameState(db.Model):
 	__tablename__='game_state'
 	
 	game_state_id = db.Column(db.Integer, primary_key=True)
-	map_state = db.Column(JSON)
+	map_state = db.Column(db.LargeBinary)
 	
 	def __init__(self, map_state):
 		self.map_state = map_state
