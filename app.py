a3d5622c07e0af06a0d38b5a673fac734a6f98d4
sdkraemer <tfcmaster9@gmail.com>
2015-12-05 11:09:39 -0700

    Added GameState back to app.py. Added first route to query DB

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import urlparse
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from flask.ext.sqlalchemy import SQLAlchemy 
 
-#app = Flask(__name__, static_url_path='')
 app = Flask(__name__, static_url_path='')
 
 # config
@@ -17,19 +16,29 @@ print os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
 
-#conn = psycopg2.connect(
-#	database=url.path[1:],
-#    user=url.username,
-#    password=url.password,
-#    host=url.hostname,
-#    port=url.port
-#)
-
+class GameState(db.Model):
+	__tablename__= 'game_state'
+	
+	game_state_id = db.Column(db.Integer, primary_key=True)
+	map_state = db.Column(db.LargeBinary)
+	
+	def __init__(self, map_state):
+		self.map_state = map_state
+	
+	def __repr__(self):
+		return '<{}>'.format(self.map_state)
+		
+print GameState.query.all()
 
 
 @app.route("/")
-def hello():
+def index():
 	return render_template("index.html")
+	
+@app.route("/game/<int:id>")
+def getGame(id):
+	game = GameState.query.filter_by(game_state_id=id).first()
+	return game.map_state
 	
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,13 +1,13 @@
-from app import db
+#from app import db
 
-class GameState(db.Model):
-	__tablename__= 'game_state'
+#class GameState(db.Model):
+#	__tablename__= 'game_state'
 	
-	game_state_id = db.Column(db.Integer, primary_key=True)
-	map_state = db.Column(db.LargeBinary)
+	#game_state_id = db.Column(db.Integer, primary_key=True)
+	#map_state = db.Column(db.LargeBinary)
 	
-	def __init__(self, map_state):
+	#def __init__(self, map_state):
 		self.map_state = map_state
 	
-	def __repr__(self):
-		return '<{}>'.format(self.map_state)
+	#def __repr__(self):
+	#	return '<{}>'.format(self.map_state)
